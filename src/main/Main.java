243f3f7679a5952716a0ecff9ca5bd1727d80b75
Manisha B <bholemr@gmail.com>
2022-03-03 14:35:29 -0500

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+        quiz.addQuestion(new Question("What is your favorite color?", "green"));
+        quiz.addQuestion(new Question("What is the answer to the ultimate question?", "42"));
+        quiz.addQuestion(new Question("How high is the St. Louis Arch in feet?", "630"));
+
+        quiz.runQuiz();
+    }
+}
